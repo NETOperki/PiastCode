2a9bb4c6841df03b6c39ddaff652b3ee3c142770
Agnieszka Madela <agnieszkamadela@gmail.com>
2017-06-10 05:28:56 +0200

    Frontend: strona główna, kontakt.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :events
-
-  root 'static_pages#index'
+  get 'contact', to: 'static_pages#contact'
+  root 'static_pages#index'	
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
