662b08aec780c4087686ccccba4f6ccf9e0c9b67
Adam Badula <adambadula1@gmail.com>
2017-06-10 08:21:00 +0200

    Łeny gotowe

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -16,6 +16,35 @@ class PlacesController < ApplicationController
   marker.lat user.latitude
   marker.lng user.longitude
 end
+
+	def category_to_name(type)
+		case type
+		when 1 
+			"Piwo"
+		when 2
+			"Jazda na rolkach"
+		when 3
+			"Bilard"
+		when 4
+			"Gry komputerowe"
+		when 5
+			"Jedzenie"
+		when 6 
+			"Szachy"
+		when 7
+			"Potańcówka"
+		when 8
+			"Bieganie"
+		when 9
+			"Jazda na rowerze"
+		when 10
+			"Koszykówka"
+		when 11
+			"Piłka nożna"
+
+
+	end
+
 	#def link
 	#	"https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=AIzaSyDsq2YWONMgdg-13qxa1IOxanjb7vs-VPs"+"&location="+get_points+"&radius="+radius
 	#end
