c9401904dc4e1f563a78b410eac624370b0dc7f9
Tomasz Król <tomicode@gmail.com>
2018-05-13 10:57:50 +0200

    Add index to user email. Check user validity.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+
+  before_save { self.email = email.downcase }
+
+  validates :name, presence: true, lenght: {maximum: 64}
+  validates :email, presence: true, lenght: {maximum: 255},
+    format: { with: VALID_EMAIL_REGEX },
+    uniqueness: {case_sensitive: false}
+
   has_many :events
   has_many :joined
   has_many :posts
