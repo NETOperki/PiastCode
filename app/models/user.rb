ceda2ff24759aa8968f1aa21af93763b1d34bd30
Tomasz Król <tomicode@gmail.com>
2018-05-13 11:02:11 +0200

    Add password fields to users.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,14 @@ class User < ApplicationRecord
 
   before_save { self.email = email.downcase }
 
-  validates :name, presence: true, lenght: {maximum: 64}
-  validates :email, presence: true, lenght: {maximum: 255},
+  validates :name, presence: true, lenght: { maximum: 64 }
+  validates :email, presence: true, lenght: { maximum: 255 },
     format: { with: VALID_EMAIL_REGEX },
-    uniqueness: {case_sensitive: false}
+    uniqueness: { case_sensitive: false }
+
+  has_secure_password
+
+  validates :password, presence: true, lenght: { minimum: 6 }
 
   has_many :events
   has_many :joined
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180513085303) do
+ActiveRecord::Schema.define(version: 20180513085825) do
 
   create_table "events", force: :cascade do |t|
     t.string "title"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 20180513085303) do
     t.string "telephone"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
