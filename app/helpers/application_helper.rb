c1ecc7fe4ac18c573af1d4b29998af23336dabd5
Tomasz Król <tomicode@gmail.com>
2018-05-15 11:44:00 +0200

    Changed app menu. Fix user model method spelling.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def item_active?(path, parse=false)
+    if (not parse and request.path == path) or (parse and request.path.include?(path)) then
+      'item active'
+    else
+      'item'
+    end
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,14 @@ class User < ApplicationRecord
 
   before_save { self.email = email.downcase }
 
-  validates :name, presence: true, lenght: { maximum: 64 }
-  validates :email, presence: true, lenght: { maximum: 255 },
+  validates :name, presence: true, length: { maximum: 64 }
+  validates :email, presence: true, length: { maximum: 255 },
     format: { with: VALID_EMAIL_REGEX },
     uniqueness: { case_sensitive: false }
 
   has_secure_password
 
-  validates :password, presence: true, lenght: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }
 
   has_many :events
   has_many :joined
